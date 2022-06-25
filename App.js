5de413419e3c26d614a2ecb1c77a7d7512ba7239
HuyLeQuoc <huylequoc3@gmail.com>
2022-06-25 15:12:47 +0700

    name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mass</Text>
+      <Text>Huy 123</Text>
       <StatusBar style="auto" />
     </View>
   );
