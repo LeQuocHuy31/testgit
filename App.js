dd7b8ceddcb7439e46f68d0fc0aba043035b5636
Huy Le Quoc <72648492+LeQuocHuy31@users.noreply.github.com>
2022-09-10 11:55:14 +0700

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Huy/Text>
+      <Text>Huy</Text>
       <StatusBar style="auto" />
     </View>
   );
