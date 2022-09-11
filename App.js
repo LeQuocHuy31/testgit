bfc52207ec1331fa957f47e844a38c9e07b8879f
HuyLeQuoc <huylequoc3@gmail.com>
2022-09-11 14:50:31 +0700

    ahdka

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button>1234 readme</Button>
+      <Button>1234 readme dad ada dada</Button>
       <StatusBar style="auto" />
         
     </View>
